22dfdec280f578ef0f65f57ac37fed7a914a3f97
김신영 <maestro.master03@gmail.com>
2025-04-09 23:03:30 +0900

    sdsdss

--- Build/Build.framework.js
+++ Build/Build.framework.js
@@ -1167,29 +1167,29 @@ var tempDouble;
 var tempI64;
 
 var ASM_CONSTS = {
- 3701708: function() {
+ 3702124: function() {
   Module["emscripten_get_now_backup"] = performance.now;
  },
- 3701763: function($0) {
+ 3702179: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 3701811: function($0) {
+ 3702227: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 3701859: function() {
+ 3702275: function() {
   performance.now = Module["emscripten_get_now_backup"];
  },
- 3701914: function() {
+ 3702330: function() {
   return Module.webglContextAttributes.premultipliedAlpha;
  },
- 3701975: function() {
+ 3702391: function() {
   return Module.webglContextAttributes.preserveDrawingBuffer;
  },
- 3702039: function() {
+ 3702455: function() {
   return Module.webglContextAttributes.powerPreference;
  }
 };
